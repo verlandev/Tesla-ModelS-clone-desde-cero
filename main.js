30f632305d477f5f67b2d42f6919ea46601d76b9
Bemart93 <bemart93@gmail.com>
2023-09-20 20:11:21 +0200

    FEAT: banner scrolling

--- main.js
+++ main.js
@@ -1,6 +1,19 @@
+const banner$$ = document.querySelector('.banner')
+
 const separatorH2$$ = document.querySelector('.separator__h2')
 const infoContainers$$ = document.querySelectorAll('.infoContainer')
 const buttonsHero$$ = document.querySelector('.buttonsHero')
+
+// Creamos una función que gestione el banner
+
+const detectingBanner = () => {
+    if(scrollY === 0 || scrollY <= 70 ) {
+        banner$$.classList.add('inactive')
+    } if(scrollY >= 100){
+        banner$$.classList.remove('inactive')
+    } 
+}
+
 
 // Creamos una función que identifique la altura del scroll a cada vez
 
@@ -21,6 +34,8 @@ function detectingScroll(elements, scrollOffset) {
 // aparezcan en distinto orden
 
 window.addEventListener('scroll', () => {
+    detectingBanner()
+
     setTimeout(() => {
         detectingScroll([separatorH2$$],450)
     },1000)
