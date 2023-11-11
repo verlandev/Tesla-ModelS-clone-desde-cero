ba550c4d2718512e0a47b0cca26ce0fe33eab0e1
Bemart93 <bemart93@gmail.com>
2023-11-11 16:41:57 +0100

    v20.4 - FIX: BEM classes for buttons in hero-section

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const banner$$ = document.querySelector(".banner");
 
 const separatorH2$$ = document.querySelector(".separator__h2");
 const infoContainers$$ = document.querySelectorAll(".hero-section__container");
-const buttonsHero$$ = document.querySelector(".hero-section-buttons");
+const buttonsHero$$ = document.querySelector(".hero-section__buttons");
 
 const captionTexts$$ = document.querySelectorAll(".captionText");
 
